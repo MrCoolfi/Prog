2d2a74d1b63f0faea635e66d8efd58d0589b6f05
705 <user@ws1013402151.pm>
2024-10-07 12:32:32 +0500

    Done

--- lab3/lab3.c
+++ lab3/lab3.c
@@ -9,6 +9,25 @@ void fill(int n, int a[])
         a[i] = rand () % 101 - 50;
 }
 
+void lol(int n, int a[]){
+    for (int i = 0; i<n; i++){
+        for (int j = i+1; j<n; j++)
+            if (a[i] == a[j]){
+                int sum = 0;
+                int proiz = 1;
+                for (int k = i+1; k <j;k++){
+                    sum += a[k];
+                    proiz = proiz * a[k];
+                }
+                a[i] = sum;
+                a[j] = proiz;
+                break;
+            }
+    }
+    
+}
+
+
 int main()
 {
     srand(time(NULL));
@@ -18,6 +37,11 @@ int main()
     int A[n];
     fill(n, A);
     int i;
+    for (i = 0; i < n; i++)
+        printf("%4d", A[i]);
+    printf("\n");
+    printf("Изменненый массив\n");
+    lol(n,A);
     for (i = 0; i < n; i++)
         printf("%4d", A[i]);
     printf("\n");
